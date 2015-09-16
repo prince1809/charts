0b23e531669093ff3b0780d10486e866f2307fb4
prince1809 <prince18pat@gmail.com>
2015-09-16 12:28:08 +0000

    Initial Test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-//app.js
+import React from 'react';
+import BindToMixin from 'react-binding';
+import _ from 'underscore';
+import json from 'react-json';
+import genie from 'genie';
+
+import {TabbedArea,TabPane,Button} from 'react-bootstrap';
+
+import SmoothLineChartDemo from './SmoothLineChartDemo.js';
+
+
+
+var patternOptions = [
+  'firstName',
+  'lastName',
+  'fullName',
+  'zipCode',
+  'zipCode5',
+  'zipCode9',
+  'city'
+];
+
+
+var App = React.createClass({
+  mixins: [BindToMixin],
+
+  render(){
+    return(<div>
+      <TabbedArea bsStyle="tabs" defaultActiveKey={1}>
+      HHHE
+      </TabbedArea>
+      </div>
+    )
+  }
+});
+
+React.render(<App />, document.getElementById('app'));
